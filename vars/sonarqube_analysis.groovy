e14b6b57c2ba6dd8ab1efe1a0cd526b1def47782
harshalp1275 <112923468+harshalp1275@users.noreply.github.com>
2025-12-11 02:57:26 +0530

    Simplify sonar-scanner command execution

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,11 +1,5 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
-      def scannerHome = tool 'SonarScanner'
-sh """
-    ${scannerHome}/bin/sonar-scanner \
-    -Dsonar.projectName=${Projectname} \
-    -Dsonar.projectKey=${ProjectKey} \
-    -X
-"""
+      sh "sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey}"
   }
 }
