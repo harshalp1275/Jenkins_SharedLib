5adb8e99e86ac133f10c5a84e224e476897fc8f3
harshalp1275 <112923468+harshalp1275@users.noreply.github.com>
2025-12-11 03:15:07 +0530

    Refactor SonarQube analysis function parameters

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,6 +1,6 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
-  def scannerHome = tool 'SonarScanner'
-  withSonarQubeEnv("${SonarQubeAPI}") {
-    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey}"
+def call(String sonarToolName, String sonarServerName, String projectKey, String projectName) {
+  def scannerHome = tool name: sonarToolName, type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+  withSonarQubeEnv(sonarServerName) {
+    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.projectName=${projectName} -Dsonar.sources=."
   }
 }
