585d6210e1acb65862cb06c5e076a6d051172943
harshalp1275 <112923468+harshalp1275@users.noreply.github.com>
2025-12-11 02:22:24 +0530

    Refactor SonarQube analysis script to use tool method

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,11 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+      def scannerHome = tool 'SonarScanner'
+sh """
+    ${scannerHome}/bin/sonar-scanner \
+    -Dsonar.projectName=${Projectname} \
+    -Dsonar.projectKey=${ProjectKey} \
+    -X
+"""
   }
 }
